464eddc438aac6157ba6b44a670c152e34deaa28
Renne Rocha <renne@rennerocha.com>
2018-01-04 08:56:56 -0200

    Created spider for sinnatrah.com.br

---
+++ homebrewsupply/homebrewsupply/spiders/sinnatrah_com_br.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+from homebrewsupply.loaders import ProductLoader
+
+
+class SinnatrahComBrSpider(CrawlSpider):
+    name = 'sinnatrah.com.br'
+    allowed_domains = [
+        'sinnatrah.com.br', 'sinnatrahcervejariaescola.commercesuite.com.br']
+    start_urls = ['http://loja.sinnatrah.com.br/']
+
+    rules = (
+        # Follow Categories
+        Rule(LinkExtractor(restrict_css='.menu')),
+
+        # Follow Pagination
+        Rule(LinkExtractor(restrict_css='.catalog-header .pagination')),
+
+        # Follow Products
+        Rule(
+            LinkExtractor(restrict_css='.catalog-content .product'),
+            callback='parse_product'
+        ),
+    )
+
+    def parse_product(self, response):
+        il = ProductLoader(response=response)
+
+        il.add_value('store', self.name)
+        il.add_value('url', response.url)
+        il.add_value('category', response.meta.get('category', 'N/A'))
+
+        il.add_css('name', '.product-name::text')
+        il.add_css('description', '#descricao *::text')
+
+        is_available = bool(response.css('.botao-nao_indisponivel'))
+        il.add_value('available', is_available)
+        il.add_xpath('price', '#preco_atual')
+
+        yield il.load_item()
